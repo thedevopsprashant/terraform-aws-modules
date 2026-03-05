f5f65454a2bb8ed662549d40e32b11be5f7c8d4e
Prashant <thedevopsprashant@gmail.com>
2026-03-05 22:59:14 +0530

    fixed ekd mod

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -9,7 +9,7 @@ resource "aws_eks_cluster" "eks" {
     subnet_ids              = var.subnet_ids
     endpoint_private_access = var.endpoint_private_access
     endpoint_public_access  = var.endpoint_public_access
-    security_group_ids      = aws_security_group.eks-cluster-sg.id
+    security_group_ids      = [aws_security_group.eks-cluster-sg.id]
   }
 
 
