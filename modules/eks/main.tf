b9ce7c9f7957f8333cfd2451c138eb75f67c0b8b
Prashant <thedevopsprashant@gmail.com>
2026-03-05 22:37:19 +0530

    fixed ekd mod

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -2,14 +2,14 @@ resource "aws_eks_cluster" "eks" {
 
   count    = var.is_eks_cluster_enabled == true ? 1 : 0
   name     = var.cluster_name
-  role_arn = var.eks_cluster_role_arn
+  role_arn = aws_iam_role.eks-cluster-role[0].arn
   version  = var.cluster_version
 
   vpc_config {
     subnet_ids              = var.subnet_ids
     endpoint_private_access = var.endpoint_private_access
     endpoint_public_access  = var.endpoint_public_access
-    security_group_ids      = var.security_group_ids
+    security_group_ids      = aws_security_group.eks-cluster-sg.id
   }
 
 
@@ -30,7 +30,7 @@ resource "aws_eks_node_group" "ondemand-node" {
   cluster_name    = aws_eks_cluster.eks[0].name
   node_group_name = "${var.cluster_name}-on-demand-nodes"
 
-  node_role_arn = var.eks_node_role_arn
+  node_role_arn = aws_iam_role.eks-nodegroup-role[0].arn
 
   scaling_config {
     desired_size = var.desired_capacity_on_demand
@@ -64,7 +64,7 @@ resource "aws_eks_node_group" "spot-node" {
   cluster_name    = aws_eks_cluster.eks[0].name
   node_group_name = "${var.cluster_name}-spot-nodes"
 
-  node_role_arn = var.eks_node_role_arn
+  node_role_arn = aws_iam_role.eks-nodegroup-role[0].arn
 
   scaling_config {
     desired_size = var.desired_capacity_spot
