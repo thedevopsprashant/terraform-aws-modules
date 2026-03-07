ebb9d0e4231ecb4cf66d4c191e8c7d23c4f7e945
Prashant <thedevopsprashant@gmail.com>
2026-03-07 11:52:10 +0530

    fixed prom

--- modules/helm/prometheus.tf
+++ modules/helm/prometheus.tf
@@ -11,34 +11,65 @@ resource "helm_release" "prometheus-helm" {
 
   timeout = 2000
 
-  values = [
-    yamlencode({
-      podSecurityPolicy = {
-        enabled = true
-      }
-      server = {
-        persistentVolume = {
-          enabled = true
-        }
-      }
-      grafana = {
-        service = {
-          type        = "LoadBalancer"
-          annotations = {
-            "service.beta.kubernetes.io/aws-load-balancer-scheme" = "internet-facing"
-          }
-        }
-      }
-      prometheus = {
-        service = {
-          type        = "LoadBalancer"
-          annotations = {
-            "service.beta.kubernetes.io/aws-load-balancer-scheme" = "internet-facing"
-          }
-        }
-      }
-    })
-  ]
+#   values = [
+#     yamlencode({
+#       podSecurityPolicy = {
+#         enabled = true
+#       }
+#       server = {
+#         persistentVolume = {
+#           enabled = true
+#         }
+#       }
+#       grafana = {
+#         service = {
+#           type        = "LoadBalancer"
+#           annotations = {
+#             "service.beta.kubernetes.io/aws-load-balancer-scheme" = "internet-facing"
+#           }
+#         }
+#       }
+#       prometheus = {
+#         service = {
+#           type        = "LoadBalancer"
+#           annotations = {
+#             "service.beta.kubernetes.io/aws-load-balancer-scheme" = "internet-facing"
+#           }
+#         }
+#       }
+#     })
+#   ]
+# }
+
+ set {
+    name  = "podSecurityPolicy.enabled"
+    value = true
+  }
+
+  set {
+    name  = "server.persistentVolume.enabled"
+    value = true
+  }
+
+  set {
+    name  = "grafana.service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "grafana.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
+    value = "internet-facing"
+  }
+
+  set {
+    name  = "prometheus.service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "prometheus.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-scheme"
+    value = "internet-facing"
+  }
 }
 
 data "kubernetes_service_v1" "prometheus_server" {
